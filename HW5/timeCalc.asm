e1c6436541b5c40c072cf96699caea507a3b44ed
= <918780@dpsk12.net>
2024-09-24 11:32:37 -0600

    functional

--- HW5/timeCalc.asm
+++ HW5/timeCalc.asm
@@ -11,11 +11,24 @@ INCLUDE C:\Irvine\Irvine32.inc
 INCLUDELIB C:\Irvine\Irvine32.lib
 
 .data
-; declare variables here
+hrsPrompt BYTE "Enter the number of hours: ",0
+minsPrompt BYTE "Enter the number of minutes: ",0
+secsPrompt BYTE "Enter the number of seconds: ",0
 
-; declare strings here
+hrsConfirm BYTE "The number of hours entered was ",0
+minConfirm BYTE "The number of minutes entered was ",0
+secConfirm BYTE "The number of seconds entered was ",0
 
-; declare arrays here
+totalMins1 BYTE "The total number of minutes is ",0
+totalMins2 BYTE " minutes.",0
+
+totalSecs1 BYTE "The total number of seconds is ",0
+totalSecs2 BYTE " seconds.",0
+
+tryagain BYTE "Try again (y/n)? ",0
+
+invalidInput BYTE "Invalid input. Try again.",0
+
 
 
 ; **********************************************************************;
@@ -32,17 +45,92 @@ INCLUDELIB C:\Irvine\Irvine32.lib
 .code
 main PROC
 
-    	; Write your code here.  
-	; Include non-syntax based comments for EACH line describing the 
-	;	contribution of the line to the overal functional goals
-	;	of the program.
+  
+START:
+	mov edx, OFFSET hrsPrompt
+	call WriteString 
+	call ReadInt
+	mov esi, eax ; store hours in esi
+
+	mov edx, OFFSET minsPrompt
+	call WriteString
+	call ReadInt
+	mov edi, eax ; store minutes in edi
+
+	mov edx, OFFSET secsPrompt
+	call WriteString
+	call ReadInt
+	mov ebx, eax ; store seconds in ebx
+
+	call Crlf
+	
+	mov edx, OFFSET hrsConfirm
+	call WriteString
+	mov eax, esi
+	call WriteInt
+	call Crlf
+
+	mov edx, OFFSET minConfirm
+	call WriteString
+	mov eax, edi
+	call WriteInt
+	call Crlf
+
+	mov edx, OFFSET secConfirm
+	call WriteString
+	mov eax, ebx
+	call WriteInt
+	call Crlf
+
+	call Crlf
+
+	
+	xor edx, edx ; clear edx
+	mov eax, ebx ;move seconds to eax
+	mov ecx, 60 
+	div ecx ; divide seconds by 60 (minutes from seconds is now in EAX)
+
+	mov ebx, edx ; store remainder seconds in ebx
+
+	imul esi, 60 ; convert hrs to mins
+
+	add edi, esi ; add minutes
+
+	add edi, eax ; add minutes 
+
+	mov eax, edi
+
+	mov edx, OFFSET totalMins1
+	call WriteString
+	call WriteInt
+	mov edx, OFFSET totalMins2
+	call WriteString
+	call Crlf
+
+	imul eax, 60
+
+	add eax, ebx ; add remainder seconds back
+
+	mov edx, OFFSET totalSecs1
+	call WriteString
+	call WriteInt
+	mov edx, OFFSET totalSecs2
+	call WriteString
+	call Crlf
 
 
-    mov eax, 0
-		MOV EAXSS 
+ASK_TRY_AGAIN:
+	call Crlf
+	mov edx, OFFSET tryagain
+	call WriteString	
+	call ReadChar
 
+.IF al == 'y' || al == 'Y'
+	call Crlf
+ 	JMP START
+.ENDIF
 
-    INVOKE ExitProcess,0
+	INVOKE ExitProcess,0
 
 main ENDP
 
