4aec366e981de334369fe73b73b85be15ffc8629
= <jeffy9745@gmail.com>
2024-10-01 11:35:31 -0600

    format

--- HW6/stringCompress.asm
+++ HW6/stringCompress.asm
@@ -14,7 +14,6 @@ INCLUDE C:\Irvine\Irvine32.inc
 INCLUDELIB C:\Irvine\Irvine32.lib
 
 .data
-
 stringPrompt BYTE "Please enter a one-line string with a maximum of 100 characters:", 0
 originalStringDisplay BYTE "Original String: ", 0
 compressedStringDisplay BYTE "Compressed String: ", 0
@@ -23,7 +22,6 @@ stringInput BYTE 101 DUP(0)
 compressedString BYTE 101 DUP(0)
 
 tryagainDisplay BYTE "Would you like to enter a new string (y/n)", 0
-
 .code
 
 ; **********************************************************************;
@@ -40,42 +38,30 @@ prompt:
     mov edx, OFFSET stringInput
     mov ecx, 101
     call ReadString
-
-    
-
     mov edx, OFFSET originalStringDisplay
     call WriteString
-
     mov edx, OFFSET stringInput
     call WriteString
-
     call Crlf
     
     mov eax, OFFSET stringInput
     mov ebx, OFFSET compressedString
 
 compressloop:
-
     mov dl, [eax]
-
     .IF (dl >= 65 && dl <= 90) || (dl >= 97 && dl <= 122)
-    
         mov [ebx], dl
         inc ebx
     .ENDIF
     inc eax
-
     cmp dl,0
     jne compressloop
 
 outputString:
-
     mov edx, OFFSET compressedStringDisplay
     call WriteString
-
     mov edx, OFFSET compressedString
     call WriteString
-
     call Crlf
 
 ASK_TRY_AGAIN:                        ; Ask user if they want to repeat the program
@@ -83,16 +69,11 @@ ASK_TRY_AGAIN:                        ; Ask user if they want to repeat the prog
     mov edx, OFFSET tryagainDisplay   ; Load try again message
     call WriteString                  ; Display try again prompt
     call ReadChar                     ; Read user response
-
     .IF al == 'y' || al == 'Y'        ; If user enters 'y' or 'Y', repeat
         call Crlf                     ; Newline for spacing
         JMP prompt                     ; Jump back 
     .ENDIF
-
     INVOKE ExitProcess, 0             ; Exit the program with status 0
-
 main ENDP
-
-
 
 END main
