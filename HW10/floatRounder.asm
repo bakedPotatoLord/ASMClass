be0adb8512134c2ca690f3db02a7e9248416d10b
bakedpotatolord <jeffy9745@gmail.com>
2024-11-06 09:24:45 -0700

    start on code

--- HW10/floatRounder.asm
+++ HW10/floatRounder.asm
@@ -10,6 +10,7 @@
 ; **********************************************************************;
 
 .386                              ; Specify 32-bit code
+.model flat,stdcall               ; Flat memory model with standard calling conventions
 .stack 4096                       ; Allocate 4 KB of stack space
 ExitProcess proto,dwExitCode:dword ; Prototype for ExitProcess from Windows API
 
@@ -17,6 +18,24 @@ INCLUDE C:\Irvine\Irvine32.inc          ; Include Irvine32 library for basic I/O
 INCLUDELIB C:\Irvine\Irvine32.lib       ; Link Irvine32 library
 
 .data
+
+prompt DB "Enter a floating point number with at least 5 decimal places and at most 100 digits: ",0
+
+precisionPrompt DB "Enter the number of decimal places to round to (1-4): ",0
+
+numberInput DB 101 DUP(0)
+
+numberDisplay DB "The number you entered is: ",0
+
+roundedNumberDisplay DB "The rounded number is: ",0
+
+prepoint DD 0
+
+postpoint DD 0
+
+invalidInputDisplay DB "Invalid input. Please try again.",0
+
+dividerDisplay DB "------------------------------------",0
 
 .code
 
@@ -31,7 +50,110 @@ INCLUDELIB C:\Irvine\Irvine32.lib       ; Link Irvine32 library
 
 main PROC
 
-  
+    floatPrompt:
+        lea edx, prompt
+        call WriteString
+
+        lea edx, numberInput
+        mov ecx, 100
+        call readString
+        
+        
+        call Crlf
+
+    inputValidate:
+        ;EDX holds curr address
+
+        .WHILE(1) ;iterate through pre- point numbers
+
+            .IF( byte ptr [edx] == 46) ; check for decimal
+                jmp afterPoint
+            .ELSEIF( byte ptr [edx] < 48 && byte ptr [edx] > 57) ; check for non-numeric
+                call invalidInput
+                jmp floatPrompt
+            .ENDIF
+
+            inc edx
+        .ENDW
+
+        afterPoint:
+
+        xor eax, eax ; eax will be decimal counter
+        .WHILE(1)
+            .if( byte ptr [edx] < 48 && byte ptr [edx] > 57) ; check for non-numeric
+                call invalidInput
+                jmp floatPrompt
+
+            .elseif( byte ptr [edx] == 0) ; check for end of string
+                .IF(eax < 5) ; if less than 5 decimal places
+                    call invalidInput
+                    JMP floatPrompt
+                .ENDIF
+                jmp validFloat
+            .ELSE ; if numeric
+                inc eax
+            .ENDIF
+
+            inc edx
+        .ENDW
+
+    invalid:
+        call invalidInput
+        jmp floatPrompt
+
+
+    validFloat:
+        lea edx, numberDisplay
+        call WriteString
+
+        lea edx, numberInput
+        call WriteString
+
+        call Crlf
+        call Crlf
+
+    askprecisionPrompt:
+
+        lea edx, precisionPrompt
+        call WriteString
+
+        call readInt
+
+        .IF(eax < 1 || eax > 4)
+            call invalidInput
+            jmp askprecisionPrompt
+        .ENDIF
+
+
+
+    outputRounded:
+        lea edx, roundedNumberDisplay
+        call WriteString
+    
+        lea edx, numberInput
+
+        .while(byte ptr [edx] != '.')
+
+
+        mov al, byte ptr [edx]
+        call WriteChar
+
+
+        inc edx
+        .endw
+
+        call Crlf
+
+        call Crlf
+
+
+        call ReadFloat
+    
+
+        JMP floatPrompt
+
+    invoke ExitProcess, 0           ; Exit program with code 0
+    
 main ENDP
 
 
