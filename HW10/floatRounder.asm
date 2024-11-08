0f9c7f7e080adb6999b25f9c07c64a9cb4f024dd
bakedpotatolord <jeffy9745@gmail.com>
2024-11-08 16:01:11 -0700

    add comments

--- HW10/floatRounder.asm
+++ HW10/floatRounder.asm
@@ -1,6 +1,8 @@
 ; **********************************************************************;
-; Program Name:   Float Rounder Program (baseConverter.asm)            ;
-; Program Description:             ;
+; Program Name:   Float Rounder Program (floatRounder.asm)              ;
+; Program Description: Rounds a floating-point number to specified      ;
+;                      decimal places, with display in scientific       ;
+;                      and decimal notation.                            ;
 ; Author:          Josiah Hamm   @bakedPotatoLord                       ;
 ; Course Number:   CSC2025X01 - Computer Architecture/Assembly Language ;
 ; Date:            10/20/2024                                           ;
@@ -15,227 +17,229 @@ INCLUDE C:\Irvine\Irvine32.inc          ; Include Irvine32 library for basic I/O
 INCLUDELIB C:\Irvine\Irvine32.lib       ; Link Irvine32 library
 
 .data
-    prompt DB "Enter a floating point number with (5-9) decimal places : ",0
-    precisionPrompt DB "Enter the number of decimal places to round to (1-4): ",0
-    numberInput DB 64 DUP(0)
-    precision DD 0
-    prepoint dd 0
-    postpoint dd 0
-    floatOutput dd 0
-    floatPass dd 0
-    bcdOut TBYTE 0
-    decimalOutput DB 21 DUP(0)
-    numberDisplay DB "The number you entered is: ",0
-    roundedNumberScientific DB "Rounded number iin scientific notation: ",0
-    roundedNumberDecimal DB "Rounded number in decimal notation: ",0
-    repeatPrompt DB "Repeat? (y/n): ",0
-    invalidInputDisplay DB "Invalid input. Please try again.",0
-    dividerDisplay DB "------------------------------------",0
+    prompt DB "Enter a floating point number with (5-9) decimal places : ",0 ; Prompt message
+    precisionPrompt DB "Enter the number of decimal places to round to (1-4): ",0 ; Precision prompt
+    numberInput DB 64 DUP(0)            ; Buffer for user input
+    precision DD 0                      ; Variable for precision level
+    prepoint dd 0                       ; Holds integer part before decimal point
+    postpoint dd 0                      ; Holds fractional part after decimal point
+    floatOutput dd 0                    ; Placeholder for output float
+    floatPass dd 0                      ; Placeholder for processing float calculations
+    bcdOut TBYTE 0                      ; BCD output for decimal formatting
+    decimalOutput DB 21 DUP(0)          ; Buffer for decimal output
+    numberDisplay DB "The number you entered is: ",0 ; Message for displaying user input
+    roundedNumberScientific DB "Rounded number in scientific notation: ",0 ; Message for scientific notation
+    roundedNumberDecimal DB "Rounded number in decimal notation: ",0 ; Message for decimal notation
+    repeatPrompt DB "Repeat? (y/n): ",0 ; Prompt for repeat option
+    invalidInputDisplay DB "Invalid input. Please try again.",0 ; Message for invalid input
+    dividerDisplay DB "------------------------------------",0 ; Divider for UI
+
 .code
 
 ; **********************************************************************;
 ; Main Procedure                                                        ;
-; Description: 
-; Input:                                                                ;
-; Output: ;
+; Description: Main procedure for handling user input, rounding float,  ;
+;              and displaying output in scientific and decimal formats. ;
+; Input:       User inputs a floating-point number and rounding         ;
+;              precision.                                               ;
+; Output:      Rounded float displayed in both scientific and decimal   ;
+;              formats.                                                 ;
 ; Register Usage:                                                       ;
 ; EAX - used as accumulator and as parameter for functions              ;
+; EBX - used to store digits of input float                             ;
+; ECX - used as loop counter and as parameter for functions             ;
+; EDX - used to pass addresses for displaying strings                   ;
+; ESI - used as loop counter for iterating over strings                 ;
+; EDI - used for writing to string outputbuffer                         ;
 ; **********************************************************************;
 
 main PROC
 
     floatPrompt:
-        lea edx, prompt
-        call WriteString
+        lea edx, prompt            ; Load the address of prompt string into EDX
+        call WriteString           ; Display the prompt to the user
 
-        lea edx, numberInput
-        mov ecx, 63
-        call readString
-        call Crlf
+        lea edx, numberInput       ; Load address of numberInput buffer
+        mov ecx, 63                ; Set max number of characters to read
+        call readString            ; Read string input from user
+        call Crlf                  ; Print a newline
 
     displayInput:
-        lea edx, numberDisplay
-        call WriteString
+        lea edx, numberDisplay     ; Load address of numberDisplay string
+        call WriteString           ; Display the numberDisplay label
 
-        lea edx, numberInput
-        call WriteString
+        lea edx, numberInput       ; Load address of user's input
+        call WriteString           ; Display the user's input
 
-        call Crlf
-        call Crlf
+        call Crlf                  ; Print a newline
+        call Crlf                  ; Print another newline
 
     askprecisionPrompt:
+        lea edx, precisionPrompt   ; Load the address of precisionPrompt string
+        call WriteString           ; Display precision prompt
 
-        lea edx, precisionPrompt
-        call WriteString
-        call readInt
-        .IF(eax < 1 || eax > 4)
-            call invalidInput
-            jmp askprecisionPrompt
+        call readInt               ; Read integer input from user
+        .IF(eax < 1 || eax > 4)    ; If input is not within [1,4]
+            call invalidInput      ; Display invalid input message
+            jmp askprecisionPrompt ; Loop back to prompt again
         .ENDIF
-        mov precision, eax ; store precision
-        call Crlf
+        mov precision, eax         ; Store valid precision input
+        call Crlf                  ; Print a newline
 
     convertloops:
-
-        lea esi, offset numberInput  ; Load address of ASCII string
-        xor eax, eax ; Clear EAX - will be accumlator
-        xor ebx, ebx ; Clear EBX - will be digit holder
-        .WHILE( byte ptr [esi] != '.')
-            mov bl, byte ptr [esi]       ; Load character from string
-            sub bl, '0'   
-            inc esi                       ; Move to next character
-            .IF(bl > 9)               
-                .CONTINUE               ; Ignore invalid characters
+        lea esi, offset numberInput; Load address of numberInput string
+        xor eax, eax               ; Clear EAX for accumulation
+        xor ebx, ebx               ; Clear EBX for digit storage
+        .WHILE(byte ptr [esi] != '.') ; Loop until decimal point is reached
+            mov bl, byte ptr [esi] ; Load character from input string
+            sub bl, '0'            ; Convert ASCII to digit
+            inc esi                ; Move to next character
+            .IF(bl > 9)            ; If character is invalid
+                .CONTINUE          ; Skip invalid characters
             .ENDIF
-            mov edx, 10
-            mul edx                       ; Multiply accumulator by 10
-            add eax,ebx                   
+            mov edx, 10            ; Set multiplier (base 10)
+            mul edx                ; Multiply accumulator by 10
+            add eax, ebx           ; Add digit to accumulator
         .ENDW
-        mov prepoint, eax
+        mov prepoint, eax          ; Store integer part in prepoint
 
-        xor eax, eax ; Clear EAX - will be accumlator
-        xor ebx, ebx ; Clear EBX - will be digit holder
-        xor cl, cl ; Clear CL - will be digit counter
-        .WHILE( byte ptr [esi] != 0)
-            mov bl, byte ptr [esi]       ; Load character from string
-            sub bl, '0'   
-            inc esi                       ; Move to next character
-            .IF(bl > 9)               
-                .CONTINUE               ; Ignore invalid characters
+        xor eax, eax               ; Clear EAX for fraction calculation
+        xor ebx, ebx               ; Clear EBX for digit storage
+        xor cl, cl                 ; Clear CL for digit count
+        .WHILE(byte ptr [esi] != 0); Loop until end of string
+            mov bl, byte ptr [esi] ; Load character
+            sub bl, '0'            ; Convert ASCII to digit
+            inc esi                ; Move to next character
+            .IF(bl > 9)            ; If character is invalid
+                .CONTINUE          ; Skip invalid characters
             .ENDIF
-            mov edx, 10
-            mul edx                       ; Multiply accumulator by 10
-            add eax,ebx                    ;
-            inc cl     ; inc digit counter
+            mov edx, 10            ; Set multiplier (base 10)
+            mul edx                ; Multiply accumulator by 10
+            add eax, ebx           ; Add digit to accumulator
+            inc cl                 ; Increment digit count
         .ENDW
-
-        mov postpoint, eax
+        mov postpoint, eax         ; Store fractional part in postpoint
 
     round:
-        finit                        ; Initialize FPU
+        finit                      ; Initialize FPU
 
-        fild dword ptr postpoint 
+        fild dword ptr postpoint   ; Load fractional part to FPU stack
 
         .IF(cl == 1)
-            mov eax, 10
+            mov eax, 10            ; Set multiplier for 1 digit
         .ELSEIF(cl == 2)
-            mov eax, 100
+            mov eax, 100           ; Set multiplier for 2 digits
         .ELSEIF(cl == 3)
-            mov eax, 1000
+            mov eax, 1000          ; Set multiplier for 3 digits
         .ELSEIF(cl == 4)
-            mov eax, 10000
+            mov eax, 10000         ; Set multiplier for 4 digits
         .ELSEIF(cl == 5)
-            mov eax, 100000
+            mov eax, 100000        ; Set multiplier for 5 digits
         .ELSEIF(cl == 6)
-            mov eax, 1000000
+            mov eax, 1000000       ; Set multiplier for 6 digits
         .ELSEIF(cl == 7)
-            mov eax, 10000000
+            mov eax, 10000000      ; Set multiplier for 7 digits
         .ELSEIF(cl == 8)
-            mov eax, 100000000
+            mov eax, 100000000     ; Set multiplier for 8 digits
         .ELSEIF(cl == 9)
-            mov eax, 1000000000
+            mov eax, 1000000000    ; Set multiplier for 9 digits
         .ENDIF
 
-        mov floatPass, eax
-        fidiv floatPass  ; divide by appropriate power of 10
-        fiadd prepoint
+        mov floatPass, eax         ; Store multiplier in floatPass
+        fidiv floatPass            ; Divide fractional part by multiplier
+        fiadd prepoint             ; Add integer part
 
-        .if( precision == 1)
-            mov eax, 10
+        .if(precision == 1)
+            mov eax, 10            ; Set precision multiplier for 1 digit
         .elseif(precision == 2)
-            mov eax, 100
+            mov eax, 100           ; Set precision multiplier for 2 digits
         .elseif(precision == 3)
-            mov eax, 1000
+            mov eax, 1000          ; Set precision multiplier for 3 digits
         .elseif(precision == 4)
-            mov eax, 10000
+            mov eax, 10000         ; Set precision multiplier for 4 digits
         .endif
 
-        mov floatPass, eax
-        fimul floatPass  ; multiply by apropriate power of 10
+        mov floatPass, eax         ; Store precision multiplier in floatPass
+        fimul floatPass            ; Multiply by precision multiplier
 
-        fistp dword ptr floatPass ; pop to floatPass
-        fild dword ptr floatPass ; push to floatPass
+        fistp dword ptr floatPass  ; Store rounded result
+        fild dword ptr floatPass   ; Reload result for display
 
-        mov floatPass, eax ; move multiplier to floatPass
-        fidiv floatPass  ; divide by appropriate power of 10
+        mov floatPass, eax         ; Copy multiplier to floatPass
+        fidiv floatPass            ; Divide by multiplier to format number
 
     displayvals:
-        lea edx, roundedNumberScientific
-        call WriteString
+        lea edx, roundedNumberScientific ; Load address of scientific notation label
+        call WriteString           ; Display label
 
-        call writeFloat
-        call Crlf
+        call writeFloat            ; Display floating point result
+        call Crlf                  ; Print newline
 
-        fimul floatPass  ; multiply by apropriate power of 10
+        fimul floatPass            ; Multiply by multiplier for BCD
 
-        FBSTP tbyte ptr bcdOut ; pop to floatPass
+        FBSTP tbyte ptr bcdOut     ; Convert FPU result to BCD format
 
-        ; mov eax, precision
-        ; inc eax
-        ; mov precision, eax
+        lea edx, bcdOut            ; Load BCD output address
+        mov ecx, edx               ; Move address to ECX for loop limit
+        add ecx, 10                ; Set ECX to end of BCD output
 
+        xor eax, eax               ; Clear EAX for BCD digit
+        xor esi, esi               ; Clear ESI for digit counter
+        lea edi, decimalOutput     ; Load address of decimal output buffer
 
-        lea edx, bcdOut
-        mov ecx, edx
-        add ecx, 10
+        .while edx < ecx           ; Loop through each BCD byte
+            mov al, byte ptr [edx] ; Load low nibble
+            and al, 0Fh            ; Isolate low nibble
+            add al, '0'            ; Convert to ASCII
+            mov byte ptr [edi+esi], al ; Store ASCII digit in buffer
+            inc esi                ; Increment digit counter
 
-        xor eax, eax ; eax will hold the BDC value
-        xor esi, esi  ; esi will be the digit counter
-        lea edi, decimalOutput ; edi will hold the decimal output
-
-        .while edx < ecx
-            mov al, byte ptr [edx]
-            and al, 0Fh ; isolate low nibble
-            add al, '0' ; convert to ASCII
-            mov byte ptr [edi+esi], al
-            inc esi
-
-            .if(precision == esi)
-                mov al, '.'
-                mov byte ptr [edi+esi], al
-                inc esi
+            .if(precision == esi)  ; If precision is reached
+                mov al, '.'        ; Insert decimal point
+                mov byte ptr [edi+esi], al ; Store decimal point
+                inc esi            ; Increment counter
             .endif
 
-            mov al, byte ptr [edx]
-            shr al, 4 ; isolate high nibble
-            add al, '0' ; convert to ASCII
-            mov byte ptr [edi+esi], al
-            inc esi
+            mov al, byte ptr [edx] ; Load high nibble
+            shr al, 4              ; Shift to low nibble position
+            add al, '0'            ; Convert to ASCII
+            mov byte ptr [edi+esi], al ; Store ASCII digit
+            inc esi                ; Increment digit counter
 
-            .if(precision == esi)
-                mov al, '.'
-                mov byte ptr [edi+esi], al
-                inc esi
+            .if(precision == esi)  ; If precision is reached
+                mov al, '.'        ; Insert decimal point
+                mov byte ptr [edi+esi], al ; Store decimal point
+                inc esi            ; Increment counter
             .endif
-            
-            inc edx
+
+            inc edx                ; Move to next BCD byte
         .endw
 
-        lea edx, roundedNumberDecimal
-        call WriteString
+        lea edx, roundedNumberDecimal ; Load decimal notation label
+        call WriteString           ; Display label
 
-        .while(esi > 0 )
-            dec esi
-            mov al, byte ptr [edi+esi]
-            call WriteChar        
+        .while(esi > 0)            ; Loop to print decimal output
+            dec esi                ; Decrement digit counter
+            mov al, byte ptr [edi+esi] ; Load digit from buffer
+            call WriteChar         ; Display digit
         .endw
 
-        call Crlf
-        call Crlf
-        call divider
+        call Crlf                  ; Print newline
+        call Crlf                  ; Print another newline
+        call divider               ; Display divider line
 
     askRepeat:
-        lea edx, repeatPrompt
-        call WriteString
+        lea edx, repeatPrompt      ; Load repeat prompt address
+        call WriteString           ; Display prompt
 
-        call ReadChar
-        .if(al == 'y' || al == 'Y')
-            call Clrscr
-            JMP floatPrompt
-        .ELSEIF (al == 'n' || al == 'N')
-            invoke ExitProcess, 0           ; Exit program with code 0
-        .ELSE
-            JMP askRepeat
+        call ReadChar              ; Read user's response
+        .if(al == 'y' || al == 'Y') ; If 'y' or 'Y' entered
+            call Clrscr            ; Clear screen
+            JMP floatPrompt        ; Go back to start
+        .ELSEIF (al == 'n' || al == 'N') ; If 'n' or 'N' entered
+            invoke ExitProcess, 0  ; Exit program with status 0
+        .ELSE                      ; If invalid input
+            JMP askRepeat          ; Re-prompt
         .ENDIF
     
 main ENDP
