cbc13f551a4b5234664657862d7201c44ba7a8c7
bakedpotatolord <jeffy9745@gmail.com>
2024-11-11 22:44:35 -0700

    complete HW11 code

--- HW11/randGuesser.asm
+++ HW11/randGuesser.asm
@@ -18,6 +18,15 @@ INCLUDELIB C:\Irvine\Irvine32.lib       ; Link Irvine32 library
 
 .data
 
+    prompt DB "Guess a number 1 to 50 : ",0
+    tooHigh DB "That number is too high. Please try again.",0
+    tooLow DB "That number is too low. Please try again.",0
+    correct DB "Correct! You guessed the number.",0
+    triesRemaining DB " tries remaining. ",0
+    noTriesRemaining DB "No tries remaining. The correct number was: ",0
+    playAgainPrompt DB "Would you like to play again? (y/n) ",0
+    invalidInputDisplay DB "Invalid input.",0
+    dividerDisplay DB "------------------------------------",0
 
 .code
 
@@ -37,6 +46,76 @@ INCLUDELIB C:\Irvine\Irvine32.lib       ; Link Irvine32 library
 
 main PROC
 
+    start:
+    mov eax , 50
+    call randomRange
+    inc eax 
+    mov ecx, eax ; ECX holds rand number
+
+    mov esi, 10 ; ESI will be attempt counter
+
+    .WHILE(esi > 0)
+
+        mov edx, offset prompt
+        call WriteString
+        call ReadDec
+
+        .IF(eax == 0)
+            lea edx, invalidInputDisplay             ; Load invalid input message
+            call WriteString                         ; Display message
+            call Crlf                                ; New line
+            jmp afterFeedback
+        .ENDIF
+
+        .IF(eax > ecx)
+            mov edx, offset tooHigh
+            call WriteString
+            call Crlf
+        .ELSEIF(eax < ecx)
+            mov edx, offset tooLow 
+            call WriteString
+            call Crlf
+        .ELSE
+            mov edx, offset correct
+            call WriteString
+            call Crlf
+            jmp askPlayAgain
+        .ENDIF
+
+        afterFeedback:
+
+        dec esi
+        mov eax, esi
+        call WriteDec
+        mov edx, offset triesRemaining
+        call WriteString
+        call Crlf
+        call Crlf
+    .ENDW
+
+    lea edx, noTriesRemaining
+    call WriteString
+
+    mov eax, ecx
+    call WriteDec
+    call Crlf
+
+    askPlayAgain:
+        mov edx, offset playAgainPrompt
+        call WriteString
+        call ReadChar
+        call Crlf
+
+        .IF( al == 'y' || al == 'Y')
+            call divider
+            jmp start
+        .ELSEIF(al == 'n' || al == 'N')
+            invoke ExitProcess, 0           ; Exit program with code 0
+        .ELSE
+            call invalidInput
+            jmp askPlayAgain
+        .ENDIF
+
 main ENDP
 
 
@@ -51,10 +130,7 @@ main ENDP
 ; **********************************************************************;
 
 invalidInput PROC uses edx
-    lea edx, invalidInputDisplay             ; Load invalid input message
-    call WriteString                         ; Display message
-    call Crlf                                ; New line
-    call divider                             ; Print divider line
+    
     ret
 invalidInput ENDP
 
